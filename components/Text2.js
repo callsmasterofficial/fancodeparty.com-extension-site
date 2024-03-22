cac246b8a734d12ce48bed198e7a6b1a576e057c
NikhilRajput <nikhilcallsmaster12@gmail.com>
2024-03-22 16:24:41 +0530

    dsbnvcs

--- components/Text2.js
+++ components/Text2.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import Text2ques from './Text2ques'
-import siteConfig from '../siteConfig'
 import InstallButton from './InstallButton'
 
-function Text2() {
+function Text2({ installBtn, home }) {
   return (
     <div className="text2Container w-screen bg-gray-100">
       <div
@@ -13,11 +12,11 @@ function Text2() {
         <div className="text2-inner_container  md:mt-0 ">
           <div className="text-box flex items-center justify-center ">
             <h3 className="primary_text_color -mt-8 mb-5 p-2 pt-5 text-2xl font-semibold md:mt-0 md:text-3xl">
-              {siteConfig.section.three.heading}
+              {home.section.three.heading}
             </h3>
           </div>
           <p className="primary_text_color text-justify">
-            {siteConfig.section.three.subHeading}
+            {home.section.three.subHeading}
           </p>
         </div>
 
@@ -25,7 +24,7 @@ function Text2() {
         <div className="main-container  m-auto mt-10 w-[100%] md:flex ">
           <div className="-mt-1 h-fit cursor-pointer md:mr-2 md:w-[50%]">
             {/* first question */}
-            <Text2ques />
+            <Text2ques home={home} />
           </div>
 
           {/* text ends here */}
@@ -40,7 +39,7 @@ function Text2() {
           </div>
         </div>
         <div className="mt-12 flex items-center   justify-center  md:w-[100%]">
-          <InstallButton />
+          <InstallButton installBtn={installBtn} />
         </div>
       </div>
     </div>
--- pages/404.js
+++ pages/404.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Error from '../components/404'
+import { getAllmeta, getHome } from '../locale/index'
 
-export async function getStaticProps() {
+export async function getStaticProps({ locale }) {
+  const allmeta = getAllmeta(locale)
+  const home = getHome(locale)
   return {
     props: {
+      home,
+      allmeta,
       meta: {
-        title:
-          'Host Fancode Watch Parties: Stream Sports with Friends in Sync ',
-        description:
-          'Host global sports streaming parties with Fancode Party Extension: real-time sync, control, live chat, HD quality. Free fun for all sports enthusiasts. ',
-        keywords:
-          'Cricket live,Fancode Party,Fancode Party Extension, Fancode Party Chrome Extension, how to use Fancode Party, install Fancode Party, Fancode Party, Fancode watch party, Fancode Party Extension, Fancode Party Chrome Extension, Watch Fancode Party, Guide to Fancode Party, how to use Fancode Party, How to Make Fancode Party, How to Make a Fancode Party, How to Watch Fancode Together,',
+        title: allmeta.foroforMetatitle,
+        description: allmeta.foroforMetadescription,
+        keywords: allmeta.homeMetakeywords,
         pageUrl: 'https://fancodeparty.com/',
         featuredImage: '/logo.png',
       },
     },
   }
 }
-function error() {
+function error({ home }) {
   return (
     <div className="error_404 ">
       <div>
-        <Navbar />
+        <Navbar installBtn={home.header.installBtn} />
       </div>
       <Error />
     </div>
